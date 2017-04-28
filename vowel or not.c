d524107440358d61142107cb403d238658a1df7a
sowndaryachithu <sowndaryachithu@gmail.com>
2017-04-28 10:01:59 +0530

    Update vowel or not.c

--- vowel or not.c
+++ vowel or not.c
@@ -1,18 +1,11 @@
 #include<stdio.h>
 int main()
 {
-char c;
-printf("enter the alphabet\n");
-scanf("%c",c);
-lowercase=(c=='a' || c=='e' || c=='i'||c=='o'||c=='u');
-uppercase=(c=='A' || c=='E' || c=='I'||c=='O'||c=='U');
-if(lowercase||uppercase)
-{
-printf("the alphebet is vowel");
-}
-else
-{ 
-printf("the alphabet is not vowel");
-}
-return 0;
+    char a;
+    printf("enter the value\n");
+    scanf("%c",&a);
+    if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U')
+    {printf("the given is vowel");}
+    else
+    {printf("the given is consonant");}
 }
