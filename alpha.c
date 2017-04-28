a81c3e81179d957f2d6b8d54ec6abee53df8ba36
sowndaryachithu <sowndaryachithu@gmail.com>
2017-04-28 09:53:54 +0530

    Create alpha.c

---
+++ alpha.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    char n;
+    printf("enter the value of n\n");
+    scanf("%c",&n);
+    if(n=='a'||n=='b'||n=='c'||n=='d'||n=='e'||n=='f'||n=='g'||n=='h'||n=='i'||n=='j'||n=='k'||n=='l'||n=='m'||n=='n'||n=='o'||n=='p'||n=='q'||n=='r'||n=='s'||n=='t'||n=='u'||n=='v'||n=='w'||n=='x'||n=='y'||n=='z')
+    {printf("the given is alphabet");}
+    else
+    {printf("the given is not an alphabet");}
+    return 0;
+}
