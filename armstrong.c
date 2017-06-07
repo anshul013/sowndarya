3742be61f6dddbdbf058daa2e1dbd99e1d7e0181
sowndaryachithu <sowndaryachithu@gmail.com>
2017-06-07 15:00:39 +0530

    Create armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+  int n, n1, rem, num=0,start,end;
+  printf("Enter start and end: ");
+  scanf("%d%d", start,end);
+  for(i=start;i<=end;i++)
+   {
+     n1=start;
+  while(n1!=0)
+  {
+      rem=n1%10;
+      num+=rem*rem*rem;
+      n1/=10;
+  }
+  if(num==n)
+    printf("%d is an Armstrong number.",n);
+  else
+    printf("%d is not an Armstrong number.",n);
+}
+}
