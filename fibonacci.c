92ec65a7165c2423bddf4c5c8153e87c5e9af042
sowndaryachithu <sowndaryachithu@gmail.com>
2017-06-07 15:45:53 +0530

    Create fibonacci.c

---
+++ fibonacci.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+ 
+int main()
+{
+    int a=0,b=1,c,i,n;
+ 
+    printf("Enter how many elements?");
+    scanf("%d",&n);
+    printf("\n%d %d",a,b);
+ 
+    for(i=2;i<n;++i)
+    {
+        c=a+b;
+        printf(" %d",c);
+        a=b;
+        b=c;
+    }
+    
+    return 0;
+}
