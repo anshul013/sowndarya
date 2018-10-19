9202eecfe4bb2864d14261e5dee842a5a0b60650
anshul013 <44233149+anshul013@users.noreply.github.com>
2018-10-19 16:01:00 +0545

    Comment and the case of 1
    Added the introductory comment and also took care of the situation if the user enters 1 as his number.

--- prime.c
+++ prime.c
@@ -1,20 +1,26 @@
+//This program is to find if the inserted number is prime or not.
 #include <stdio.h>
 int main()
 {
   int n, i, f=0;
   printf("Enter a positive integer: ");
   scanf("%d",&n);
-  for(i=2;i<=n/2;++i)
+  if(n!=1)
   {
-      if(n%i==0)
-      {
-          f=1;
-          break;
-      }
+   for(i=2;i<=n/2;++i)
+   {
+       if(n%i==0)
+       {
+           f=1;
+           break;
+       }
+   }
+   if (f==0)
+       printf("%d is a prime number.",n);
+   else
+       printf("%d is not a prime number.",n);
   }
-  if (f==0)
-      printf("%d is a prime number.",n);
   else
-      printf("%d is not a prime number.",n);
-  return 0;
+       printf("%d is neither a prime nor a composite number.",n);
+   return 0;
 }
