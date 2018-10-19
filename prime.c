e2b89ddd218d541054345a11038515a865a29354
anshul013 <44233149+anshul013@users.noreply.github.com>
2018-10-19 16:06:46 +0545

    Added comment and case of input as 1

--- prime.c
+++ prime.c
@@ -7,20 +7,22 @@ int main()
   scanf("%d",&n);
   if(n!=1)
   {
-   for(i=2;i<=n/2;++i)
-   {
-       if(n%i==0)
-       {
-           f=1;
-           break;
-       }
-   }
-   if (f==0)
-       printf("%d is a prime number.",n);
-   else
-       printf("%d is not a prime number.",n);
+      for(i=2;i<=n/2;++i)
+      {
+            if(n%i==0)
+            {
+                f=1;
+                break;
+            }
+      }
+      if (f==0)
+          printf("%d is a prime number.",n);
+      else
+          printf("%d is not a prime number.",n);
   }
   else
+  {
        printf("%d is neither a prime nor a composite number.",n);
+  }
    return 0;
 }
